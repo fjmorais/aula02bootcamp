8b769f6b68451e6d268303a2f6764b3a3d4c7929
fjmorais <fabiano.morais@outlook.com>
2024-02-20 12:43:03 -0300

    Adicionar os dados do exercicios

--- exercicios.py
+++ exercicios.py
@@ -29,3 +29,10 @@ area_do_circulo_formata = "{:.2f}".format(area_do_circulo)
 print(area_do_circulo)
 print(area_do_circulo_formata )
 print(f"{area_do_circulo:.2f}")
+
+#Exercicio 15
+data_do_usuario = input("Insira uma data no formato dd/mm/aaaa:")
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 é o: {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 é o: {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 é o: {lista_de_dia_mes_ano[2]}")
